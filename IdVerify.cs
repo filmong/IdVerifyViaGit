a3734e9e46248566d101aebc4a397ad01e9281b6
Filmon Ghebre <fiha00@gmail.com>
2018-12-13 10:45:26 -0800

    Have a dummy apis

--- IdVerify.cs
+++ IdVerify.cs
@@ -24,69 +24,15 @@ namespace IdVerify
         const string AuthorizationHeader = "Authorization";
         const string AuthorizationHeaderValue = "";
 
-        [FunctionName("doIdVerify")]
+        [FunctionName("PerformIdVerify")]
         public static async Task<IActionResult> Run1Async(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req,
             TraceWriter log)
         {
-            log.Info("HTTP triggered request is started processing...");
-
-            // client input
-            string country = ExtractValue(req, "country");
-            string idtype = ExtractValue(req, "idtype");
-
-            var formCollection = req.Form;
-
-            // client input, read pic file.
-            string base64FrontImageString = string.Empty;
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                if (req.HasFormContentType)
-                {
-                    var files = formCollection.Files;
-                    // files["frontsideImage"].CopyTo(memoryStream);
-
-                    files.GetFile("frontsideImage").CopyTo(memoryStream);
-                    base64FrontImageString = Convert.ToBase64String(memoryStream.ToArray());
-                }
-            }
-
-            // build httpRequest to Vendor API
-            HttpRequestMessage requestMessage = new HttpRequestMessage();
-            var headers = requestMessage.Headers;
-            headers.Add(AcceptHeader, AcceptHeaderApplicationJson);
-            headers.Add(UserAgentHeader, UserAgentValue);
-            headers.Add(AuthorizationHeader, AuthorizationHeaderValue);
-            requestMessage.Method = HttpMethod.Post;
-            string performVerifyURLConfigElement = GetAppConfigValue(log, "PerformNetVerifyURI");
-            requestMessage.RequestUri = new Uri(performVerifyURLConfigElement);
-            requestMessage.Version = HttpVersion.Version11;
-
-            // body on POST to Id Verify, create the whole request content.
-            // merchantIdScanReference is the id the service uses for a request to submit to NetVerify.
-            Guid merchantIdScanReference = Guid.NewGuid();
-
-            string content = $"{{\"merchantIdScanReference\": \"{merchantIdScanReference}\", \"frontsideImage\": \"{base64FrontImageString}\", \"enabledFields\":\"idFirstName,idLastName\", \"country\": \"{country}\", \"idType\": \"{idtype}\"}}";
-            StringContent bodyContent = new StringContent(content, System.Text.Encoding.UTF32, "application/json");
-
-            requestMessage.Content = bodyContent;
-
-            // send the request
-            HttpClient httpClient = new HttpClient();
-            log.Info($"Request send to requestMessage.RequestUri is {requestMessage.RequestUri}");
-            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(requestMessage);
-            var responseString = await httpResponseMessage.Content.ReadAsStringAsync();
-            log.Info($"Response from requestMessage.RequestUri is {responseString}");
-
-            // TODO: process result
-
-            // TODO: update patron for member id is verified
-
+            log.Info("HTTP triggered request is started processing...");            
 
             log.Info("HTTP request is completed processing.");
-            return httpResponseMessage.IsSuccessStatusCode
-                ? (ActionResult)new OkObjectResult(responseString)
-                : new BadRequestObjectResult(responseString);
+            return new OkObjectResult("success");
         }
 
         [FunctionName("getIdVerify")]
@@ -94,39 +40,12 @@ namespace IdVerify
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req,
             TraceWriter log)
         {
-            log.Info("HTTP triggered, request processing started...");
-
-            // client input
-            string scanIdReference = req.Query["scanId"];
-            log.Info($"scanIdReference {scanIdReference}");
-
-            // build httpRequest to Vendor API
-            HttpRequestMessage requestMessage = new HttpRequestMessage();
-            var headers = requestMessage.Headers;
-            headers.Add(AcceptHeader, AcceptHeaderApplicationJson);
-            headers.Add(UserAgentHeader, UserAgentValue);
-            headers.Add(AuthorizationHeader, AuthorizationHeaderValue);
-            requestMessage.Method = HttpMethod.Get;
-            string getNetVerifyURL = GetAppConfigValueAndFormat(log, "GetNetVerifyURI", scanIdReference);
-
-            requestMessage.RequestUri = new Uri(getNetVerifyURL);
-            requestMessage.Version = HttpVersion.Version11;
-
-            // send the request
-            HttpClient httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
-            HttpResponseMessage httpResponseMessage = response;
-
-            // TODO: process result
-
-            // TODO: update patron for member id is verified
+            log.Info("HTTP triggered, request processing started...");       
 
             log.Info("Http request is processed successfully.");
 
             // return to client appropriate object result saying request is processed
-            return httpResponseMessage.IsSuccessStatusCode
-                ? (ActionResult)new OkObjectResult(await httpResponseMessage.Content.ReadAsStringAsync())
-                : new BadRequestObjectResult(await httpResponseMessage.Content.ReadAsStringAsync());
+            return new OkObjectResult("success");
         }
         /// <summary>
         /// 
